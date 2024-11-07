5388ea30b6cde101cd1b3ce9dbdd72715999ff03
조승주 <tmdwn3318@gmail.com>
2024-11-07 16:54:53 +0900

    AuthenticationService 구현 (로그인, 로그아웃)

--- src/main/java/com/nhnacademy/springcorefinal/account/service/AuthenticationService.java
+++ src/main/java/com/nhnacademy/springcorefinal/account/service/AuthenticationService.java
@@ -1,17 +1,38 @@
 package com.nhnacademy.springcorefinal.account.service;
 
 import com.nhnacademy.springcorefinal.account.dto.Account;
+import com.nhnacademy.springcorefinal.common.dataparser.DataParser;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
-
+@Slf4j
+@Component
+@Getter
+@RequiredArgsConstructor
 public class AuthenticationService {
 
-    private Account currentAccount;
+    private final DataParser dataParser;
+
+    // 현재 로그인 된 계정 (null -> 로그인 상태가 아님)
+    private Account currentAccount = null;
+
 
     public Account login(Long id, String password) {
+        // 로그인: 계정 목록에서 id와 암호가 일치하는 계정 정보를 찾아서 반환한다.
+        for(Account account : dataParser.accounts()){
+            if(account.getId() == id && account.getPassword().equals(password)){
+                return account;
+            }
+        }
         return null;
     }
 
     public void logout() {
+        // 로그아웃: 현재 계정을 제거한다.
+        currentAccount = null;
+
     }
 
 
--- src/main/java/com/nhnacademy/springcorefinal/common/runner/AppRunner.java
+++ src/main/java/com/nhnacademy/springcorefinal/common/runner/AppRunner.java
@@ -1,22 +1,22 @@
-package com.nhnacademy.springcorefinal.common.runner;
-
-import com.nhnacademy.springcorefinal.common.dataparser.DataParser;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
-
-@Slf4j
-@Component
-@RequiredArgsConstructor
-public class AppRunner implements ApplicationRunner {
-
-    private final DataParser dataParser;
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-
-
-    }
-}
+//package com.nhnacademy.springcorefinal.common.runner;
+//
+//import com.nhnacademy.springcorefinal.common.dataparser.DataParser;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class AppRunner implements ApplicationRunner {
+//
+//    private final DataParser dataParser;
+//
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//
+//
+//    }
+//}
